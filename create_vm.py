eff367bab699978b2de2f43fb0bc84916645e911
Herve <dockerlite@gmail.com>
2018-12-17 05:04:53 +0100

    remove set password

--- create_vm.py
+++ create_vm.py
@@ -9,5 +9,5 @@ number_of_vm=input("Nbr_of_vm ? ")
 for i in range(1, int(number_of_vm)+1):
     vm_name = name + str(i)
     api.request("vm", "create",
-                {'hostname': vm_name, 'plan_id': 4, 'region': 'roubaix', 'set_password': 'lawn-vex', 'image_id': 85048,
+                {'hostname': vm_name, 'plan_id': 4, 'region': 'roubaix', 'image_id': 85048,
                  'storage': 70})
